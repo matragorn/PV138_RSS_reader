12a4480aa91bf9f3dc39c97e5af0dcd27cef799f
lukasFindura <lukas.findura@gmail.com>
2015-05-21 12:03:20 +0200

    Unit tests
    Feed NullException unit tests

---
+++ PV138_RSS_Reader/UnitTests/FeedTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PV138_RSS_Reader;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FeedTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullTitleGetException()
+        {
+            string FeedUrl = "www.someURL.com";
+            string Title = null;
+            string MainPageLink = "www.someWebAdress.com";
+            string Description = "Amazing Feed!";
+
+            Feed myFeed = new Feed(FeedUrl, Title, MainPageLink, Description);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullURLGetException()
+        {
+            string FeedUrl = null;
+            string Title = "News";
+            string MainPageLink = "www.someWebAdress.com";
+            string Description = "Amazing Feed!";
+
+            Feed myFeed = new Feed(FeedUrl, Title, MainPageLink, Description);
+        }
+    }
+}
