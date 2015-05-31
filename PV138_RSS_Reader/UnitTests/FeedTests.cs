5a5b225613ff90937c439b4f71ab39d2fde820b8
JirkaVrbka <vrbka.jirka@gmail.com>
2015-05-31 23:11:47 +0200

    Feedtesty

--- PV138_RSS_Reader/UnitTests/FeedTests.cs
+++ PV138_RSS_Reader/UnitTests/FeedTests.cs
@@ -30,5 +30,36 @@ namespace UnitTests
 
             Feed myFeed = new Feed(FeedUrl, Title, MainPageLink, Description);
         }
+
+        [TestMethod]
+        public void EqualFeeds()
+        {
+            string FeedUrl = "www.someURL.com";
+            string Title = "News";
+            string MainPageLink = "www.someWebAdress.com";
+            string Description = "Amazing Feed!";
+
+            Feed feedOne = new Feed(FeedUrl, Title, MainPageLink, Description);
+            Feed feedTwo = new Feed(FeedUrl, Title, MainPageLink, Description);
+
+            Assert.AreEqual(true, feedOne.Equals(feedTwo));
+            Assert.AreEqual(true, feedTwo.Equals(feedOne));            
+        }
+
+        [TestMethod]
+        public void NotEqualFeeds()
+        {
+            string FeedUrlOne = "www.someURL.com";
+            string FeedUrlTwo = "www.anotherURL.com";
+            string Title = "News";
+            string MainPageLink = "www.someWebAdress.com";
+            string Description = "Amazing Feed!";
+
+            Feed feedOne = new Feed(FeedUrlOne, Title, MainPageLink, Description);
+            Feed feedTwo = new Feed(FeedUrlTwo, Title, MainPageLink, Description);
+
+            Assert.AreNotEqual(true, feedOne.Equals(feedTwo));
+            Assert.AreNotEqual(true, feedTwo.Equals(feedOne));
+        }
     }
 }
