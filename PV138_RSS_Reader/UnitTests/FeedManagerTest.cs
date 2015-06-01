1b7fc4cbc97991c8dfa3901a2ac19747929a9ae9
JirkaVrbka <vrbka.jirka@gmail.com>
2015-06-01 13:53:01 +0200

    test

---
+++ PV138_RSS_Reader/UnitTests/FeedManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PV138_RSS_Reader;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FeedManagerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullStorage()
+        {
+            FeedManager manager = new FeedManager(null);
+        }
+
+
+    }
+}
