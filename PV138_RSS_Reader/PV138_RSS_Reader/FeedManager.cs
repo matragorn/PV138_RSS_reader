8994ffd9ae578d1f5195de7a0185d1d8b24bc937
jkbmat <jkb.matuska@gmail.com>
2015-06-01 23:28:52 +0200

    Revert "Reorganizovana extension pre nacitavanie stringu s diakritikou z webu"
    This reverts commit 3c007bbf958a42c840e467f4f89e085e67447a11.

--- PV138_RSS_Reader/PV138_RSS_Reader/FeedManager.cs
+++ PV138_RSS_Reader/PV138_RSS_Reader/FeedManager.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using PV138_RSS_Reader.Extensions;
 
 namespace PV138_RSS_Reader
 {
@@ -213,5 +212,68 @@ namespace PV138_RSS_Reader
 
 
 
+    }
+
+    
+
+    /// <summary>
+    /// DownloadStringAwareOfEncoding od Konamiman
+    /// zo http://stackoverflow.com/questions/4716470/webclient-downloadstring-returns-string-with-peculiar-characters
+    /// 
+    /// Nacita string z url v spravnom encodingu
+    /// </summary>
+    public static class WebUtils
+    {
+        public static Encoding GetEncodingFrom(
+            NameValueCollection responseHeaders,
+            Encoding defaultEncoding = null)
+        {
+            if (responseHeaders == null)
+                throw new ArgumentNullException("responseHeaders");
+
+            //Note that key lookup is case-insensitive
+            var contentType = responseHeaders["Content-Type"];
+            if (contentType == null)
+                return defaultEncoding;
+
+            var contentTypeParts = contentType.Split(';');
+            if (contentTypeParts.Length <= 1)
+                return defaultEncoding;
+
+            var charsetPart =
+                contentTypeParts.Skip(1).FirstOrDefault(
+                    p => p.TrimStart().StartsWith("charset", StringComparison.InvariantCultureIgnoreCase));
+            if (charsetPart == null)
+                return defaultEncoding;
+
+            var charsetPartParts = charsetPart.Split('=');
+            if (charsetPartParts.Length != 2)
+                return defaultEncoding;
+
+            var charsetName = charsetPartParts[1].Trim();
+            if (charsetName == "")
+                return defaultEncoding;
+
+            try
+            {
+                return Encoding.GetEncoding(charsetName);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new InvalidOperationException(
+                    "The server returned data in an unknown encoding: " + charsetName,
+                    ex);
+            }
+        }
+    }
+
+    public static class WebClientExtensions
+    {
+        public static string DownloadStringAwareOfEncoding(this WebClient webClient, string uri)
+        {
+            var rawData = webClient.DownloadData(uri);
+            var encoding = WebUtils.GetEncodingFrom(webClient.ResponseHeaders, Encoding.UTF8);
+            return encoding.GetString(rawData);
+        }
     }
 }
