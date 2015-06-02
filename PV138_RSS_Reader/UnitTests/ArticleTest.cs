031e10bfeb3e66bdb2d136160bda46de990301b3
JirkaVrbka <vrbka.jirka@gmail.com>
2015-06-02 05:21:31 +0200

    FinalTestPackage

---
+++ PV138_RSS_Reader/UnitTests/ArticleTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PV138_RSS_Reader;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ArticleTest
+    {
+
+        [TestMethod]
+        public void ArticleEqualArticles()
+        {
+            string Title = "title";
+            string Url = "www.someUrl.com";
+            string Description = "bla";
+            DateTime Time = new DateTime(2015, 5, 20);
+
+
+            Article myArticle1 = new Article(Title, Url, Description, Time);
+            Article myArticle2 = new Article(Title, Url, Description, Time);
+            Article myArticle3 = new Article(Title, Url, Description, new DateTime(1999, 1, 1));
+
+            Assert.AreEqual(true, myArticle1.Equals(myArticle2));
+            Assert.AreEqual(true, myArticle2.Equals(myArticle1));
+            Assert.AreEqual(true, myArticle1.Equals(myArticle3));
+
+
+        }
+
+        [TestMethod]
+        public void ArticleNotEqualArticles()
+        {
+            string Title = "title";
+            string AnotherTitle = "anotherTitle";
+            string Url = "www.someUrl.com";
+            string AnotherUrl = "anotherUrl";
+            string Description = "bla";
+            DateTime Time = new DateTime(2015, 5, 20);
+
+            Article myArticle1 = new Article(Title, Url, Description, Time);
+            Article myArticle2 = new Article(AnotherTitle, Url, Description, Time);
+            Article myArticle3 = new Article(Title, AnotherUrl, Description, Time);
+
+            Assert.AreNotEqual(true, myArticle1.Equals(myArticle2));
+            Assert.AreNotEqual(true, myArticle2.Equals(myArticle1));
+            Assert.AreNotEqual(true, myArticle2.Equals(myArticle3));
+        }
+    }
+}
---
+++ PV138_RSS_Reader/UnitTests/CategoryTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PV138_RSS_Reader;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CategoryTest
+    {
+
+        [TestMethod]
+        public void CategoryDifferentConstructors()
+        {
+            Category category1 = new Category(null);
+
+            Category category2 = new Category();
+
+            Category category3 = new Category(null, "Stars", 1);
+
+        }
+
+        [TestMethod]
+        public void CategoryAddRemoveFeed()
+        {
+            List<IFeed> feedList = new List<IFeed>();
+            Feed feed1 = new Feed("www.url.com", "title", "www.link.com", "something");
+            Feed feed2 = new Feed("www.url2.com", "title2", "www.link2.com", "something2");
+            Feed feed3 = new Feed("www.url3.com", "title3", "www.link3.com", "something3");
+
+            feedList.Add(feed1);
+            feedList.Add(feed2);
+
+            Category category = new Category(feedList);
+
+            category.RemoveFeed(feed3);
+
+            Assert.AreEqual(2, category.Feeds.Count);
+
+            category.AddFeed(feed3);
+
+            Assert.AreEqual(3, category.Feeds.Count);
+
+            category.RemoveFeed(feed2);
+
+            Assert.AreEqual(2, category.Feeds.Count);
+
+        }
+
+    }
+}
